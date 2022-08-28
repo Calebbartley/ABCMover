46a8aae289879eb8884ad3f1ab3250f9e1014fb4
Caleb Bartley <cajo9@Mac-mini-7.attlocal.net>
2022-08-28 16:07:29 -0400

    Card component added

--- abc_mover/src/App.js
+++ abc_mover/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css'
+import Body from './components/Body';
+
 
 
 
@@ -9,6 +11,8 @@ function App() {
   return (
     <div className='app' >
       <Header/>
+      <Body/>
+      
     </div>
   );
 }
---
+++ abc_mover/src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from './components/Cards';
+
+function Body() {
+  return (
+    <div className='bodyContainer' >
+        <div className='cardContainer'>
+        <Cards/>
+        </div>
+    </div>
+  )
+}
+
+export default Body
